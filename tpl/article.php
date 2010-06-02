b6788eaa17bd7b9f1d266dfb928a850c30b0e358
nifty <nifty02@gmail.com>
2010-06-02 15:14:41 +0200

    new pages

---
+++ tpl/article.php
@@ -0,0 +1,41 @@
+<div id="mainframe" class="clearfix">
+	<div id="content">
+		<nav id="subnav" class="clearfix">
+			<p>
+				<a href="#">Hjem</a> / <span>Program</span>
+			</p>
+		</nav>
+		<article id="article">
+			<h1 class="heading">Program</h1>
+			<aside id="article-meta">
+				<section class="byline">
+					<h2>Lisa berg</h2>
+					<img src="img/article-avatar.png" alt="Lisa Berg" />
+					<time>26 April, 2010</time>
+				</seaction>
+				<section class="share">
+					<h2>Del denne artikkelen</h2>
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</section>
+			</aside>
+			<div class="entry">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dignissim urna vitae diam molestie sit amet feugiat ligula fermentum. Mauris scelerisque, justo at vehicula mollis, purus sem dictum tellus, rutrum volutpat dolor urna et ante. Praesent pulvinar ante eu enim iaculis a hendrerit justo tempor. Donec id turpis at nunc vestibulum vehicula eu pellentesque lectus. Proin cursus, augue vel lacinia lacinia, lorem lectus molestie odio, sit amet interdum massa leo at felis. Pellentesque quis risus nunc. Nulla ultricies iaculis molestie. Mauris sodales consectetur leo ut bibendum. Sed vitae neque libero, tristique semper velit. Praesent venenatis pharetra dui, at rhoncus dolor euismod at. Nulla consectetur pretium justo, a placerat lectus posuere bibendum. Donec elementum aliquam pretium. Nunc hendrerit massa id tortor dapibus facilisis. Sed pellentesque tortor a velit tempor in sodales turpis posuere.</p>
+				<p>Vestibulum non erat a nulla ultrices ullamcorper. Sed pulvinar sem ac sapien convallis nec laoreet ante ornare. Nulla et mi ipsum. Proin et enim eget nulla tempor gravida eget tristique magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In sollicitudin nunc non ligula feugiat vulputate. Sed vel nibh nulla, vitae tristique tellus. In molestie, nisl eu semper sodales, arcu ligula posuere sem, rutrum suscipit ante enim eu ligula. Vestibulum vel condimentum risus. Nunc laoreet, urna mattis congue tristique, nibh erat hendrerit nibh, vel rutrum nisl urna quis massa. Maecenas turpis lorem, aliquam eget blandit ac, sollicitudin eget neque. Vivamus porta nisl porta elit tincidunt semper.</p>
+				<p>Sed bibendum hendrerit odio, nec vehicula magna vehicula a. Morbi feugiat vehicula nisi luctus hendrerit. Vivamus sed luctus risus. In non nisi neque, tincidunt viverra enim. In sodales tortor sit amet ligula suscipit interdum. Nullam molestie ipsum lectus, ut rutrum nulla. Nam sed massa elit. Aenean pulvinar dapibus erat eget interdum. Quisque ut consequat urna. Aenean sollicitudin urna vel nunc sodales dapibus. Quisque ut justo neque, sit amet mollis orci. Proin fringilla lobortis urna sit amet faucibus. Quisque mattis gravida orci et dictum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur nec tellus libero, ut pharetra urna. Duis ac gravida nibh. Vivamus at arcu at metus varius ultrices ut ut arcu. Donec ligula risus, condimentum quis adipiscing a, scelerisque eu massa. Vivamus at nisi mi, ut scelerisque est.</p>
+			</div>
+		</article>
+	</div>
+	
+	<aside id="right-col" class="wide">
+		<?php
+		include 'inc/rightcol/order.inc';
+		include 'inc/rightcol/artists.inc';
+		include 'inc/rightcol/playlists.inc';
+		include 'inc/rightcol/untouched.inc';
+		?>
+	</aside>
+</div>
---
+++ tpl/program.php
@@ -0,0 +1,101 @@
+<div id="mainframe" class="clearfix">
+	<div id="content">
+		<nav id="subnav" class="clearfix">
+			<p>
+				<a href="#">Hjem</a> / <span>Program</span>
+			</p>
+		</nav>
+		<article id="article">
+			<h1 class="heading">Program</h1>
+			<div class="entry">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse orci elit, aliquam non tempus condimentum, fermentum id neque. Ut nibh nisi, sollicitudin suscipit molestie ac, vehicula in neque. Aenean sem velit, convallis sit amet malesuada ut, ullamcorper at magna. Aliquam quis risus felis, vitae mattis nulla. Sed vulputate augue ut sapien tincidunt luctus. Integer pharetra sodales dui, id elementum turpis pulvinar in. Ut consectetur pulvinar erat, vitae interdum nunc rutrum ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean luctus porttitor pretium. Sed ornare libero non est adipiscing nec elementum felis hendrerit. </p>
+				
+				<section id="programs">
+					<div id="printer">
+						<a href="#"><span class="text">Skriv ut program</span><span class="arrow"></span></a>
+						<div class="popdown">
+							<form action="">
+								<h2>Hva vil du skrive ut?</h2>
+								<ul>
+									<li>
+										<label>
+											<input type="checkbox" name="printday[]" value="" />
+											<span>Torsdag 24. Juni</span>
+										</label>
+									</li>
+									<li>
+										<label>
+											<input type="checkbox" name="printday[]" value="" />
+											<span>Fredag 25. Juni</span>
+										</label>
+									</li>
+									<li>
+										<label>
+											<input type="checkbox" name="printday[]" value="" />
+											<span>Lørdag 26. Juni</span>
+										</label>
+									</li>
+								</ul>
+								<input type="submit" value="Skriv ut" />
+							</form>
+						</div>
+					</div>
+					<?php for ($i = 0; $i < 3; $i++) { ?>
+						<table class="program">
+							<caption>Torsdag 24. Juni</caption>
+							<col span="1" class="artist-name" />
+							<thead>
+								<tr>
+									<th>Artist<span class="arrow"></span></th>
+									<th>Scene<span class="arrow"></span></th>
+									<th class="time">Tidspunkt<span class="arrow"></span></th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>The Royalties (NO)</td>
+									<td>Enga (hovedscenen)</td>
+									<td class="time">20:00</td>
+								</tr>
+								<tr>
+									<td>Burning Rubber (NO)</td>
+									<td>Skogholtet (Urørt og nytt)</td>
+									<td class="time">20:00</td>
+								</tr>
+								<tr>
+									<td>Bertine Zetlitz (NO)</td>
+									<td>Fjellhallen (klubbscenen)</td>
+									<td class="time">19:30</td>
+								</tr>
+								<tr>
+									<td>Lukas Kasha (NO)</td>
+									<td>Skogholtet (Urørt og nytt)</td>
+									<td class="time">18:30</td>
+								</tr>
+								<tr>
+									<td>Every Minute (NO)</td>
+									<td>Enga (hovedscenen)</td>
+									<td class="time">18:45</td>
+								</tr>
+								<tr>
+									<td>Every Minute (NO)</td>
+									<td>Fjellhallen (klubbscenen)</td>
+									<td class="time">22:15</td>
+								</tr>
+							</tbody>
+						</table>
+					<?php } ?>
+				</section>
+			</div>
+		</article>
+	</div>
+	
+	<aside id="right-col" class="wide">
+		<?php
+		include 'inc/rightcol/order.inc';
+		include 'inc/rightcol/artists.inc';
+		include 'inc/rightcol/playlists.inc';
+		include 'inc/rightcol/untouched.inc';
+		?>
+	</aside>
+</div>
